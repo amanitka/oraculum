ed6b6d13b30e72ea241095e70af1afdc34c15c79
MISTRXPC\ludek <ludek.p@email.cz>
2026-05-23 18:08:28 +0200

    Initialize project structure with Maven, Spring Boot, Vaadin, and configuration files.

---
+++ src/test/java/com/oraculum/OraculumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oraculum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OraculumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
