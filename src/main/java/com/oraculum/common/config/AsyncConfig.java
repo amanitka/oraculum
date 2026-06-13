dcde295ebb9ef5257076c3dc21790b91d89e0a97
ludek <ludek.pokorny@hotmail.com>
2026-06-13 21:48:31 +0200

    Add primary `taskExecutor` bean with virtual thread support in AsyncConfig

--- src/main/java/com/oraculum/common/config/AsyncConfig.java
+++ src/main/java/com/oraculum/common/config/AsyncConfig.java
@@ -2,12 +2,22 @@ package com.oraculum.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.concurrent.Executor;
 
 @Configuration
 public class AsyncConfig {
+
+    @Primary
+    @Bean(name = "taskExecutor")
+    public Executor taskExecutor() {
+        SimpleAsyncTaskExecutor executor = new SimpleAsyncTaskExecutor("taskExecutor-");
+        executor.setVirtualThreads(true);
+        return executor;
+    }
 
     @Bean(name = "analysisExecutor")
     public Executor analysisExecutor() {
