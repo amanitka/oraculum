771481b36575927cf1e843afe970f070486d7cb5
ludek <ludek.pokorny@hotmail.com>
2026-06-14 00:24:08 +0200

    Add Lumo and custom stylesheet configuration to Oraculum application

--- src/main/java/com/oraculum/OraculumApplication.java
+++ src/main/java/com/oraculum/OraculumApplication.java
@@ -2,6 +2,8 @@ package com.oraculum;
 
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.component.page.ColorScheme;
+import com.vaadin.flow.theme.lumo.Lumo;
+import com.vaadin.flow.component.dependency.StyleSheet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
@@ -13,6 +15,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @EnableAsync
 @ColorScheme(ColorScheme.Value.LIGHT_DARK)
+@StyleSheet(Lumo.STYLESHEET)
+@StyleSheet("./themes/oraculum/styles.css")
 public class OraculumApplication implements AppShellConfigurator {
 
     static void main(String[] args) {
