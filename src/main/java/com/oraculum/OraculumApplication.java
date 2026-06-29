48bc99a0253141eb31dbbb1ae77690ddd67dda83
ludek <ludek.pokorny@hotmail.com>
2026-06-29 17:19:20 +0200

    Move theme assets to `META-INF/resources` and update path references:
    - Relocate `styles.css` and `theme.json` to comply with Vaadin's recommended structure.
    - Adjust stylesheet reference in `OraculumApplication`.
    - Remove unused theme configuration in `application.yaml`.

--- src/main/java/com/oraculum/OraculumApplication.java
+++ src/main/java/com/oraculum/OraculumApplication.java
@@ -17,7 +17,7 @@ import com.vaadin.flow.component.dependency.StyleSheet;
 @EnableAsync
 @Push
 @ColorScheme(ColorScheme.Value.LIGHT_DARK)
-@StyleSheet("./themes/oraculum/styles.css")
+@StyleSheet("/themes/oraculum/styles.css")
 public class OraculumApplication implements AppShellConfigurator {
 
     static void main(String[] args) {
