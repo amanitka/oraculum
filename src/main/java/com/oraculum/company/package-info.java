f54f116fdeff4e5351e8306984b22e1fedb8271b
Ludek Pokorny <ludek.pokorny@theifactory.com>
2026-07-05 11:06:58 +0200

    Add missing newline at end of files throughout the codebase.

--- src/main/java/com/oraculum/company/package-info.java
+++ src/main/java/com/oraculum/company/package-info.java
@@ -2,7 +2,9 @@
  * The company module, responsible for managing core company data, financial statements,
  * and market information.
  *
- * @see com.oraculum.company.api.CompanyApi
+ * @see com.oraculum.company.api.CompanyMetadataApi
  */
-@org.springframework.modulith.NamedInterface("api")
+@NamedInterface("api")
 package com.oraculum.company;
+
+import org.springframework.modulith.NamedInterface;
