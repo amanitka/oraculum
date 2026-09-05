6a325fbd89023d50e6dc08e92f8e209ed6b3c335
ludek <ludek.pokorny@hotmail.com>
2026-09-05 22:40:48 +0200

    Refactor SecHoldingFileLoadServiceImplTest: reorder imports for consistency and add `anyString` matcher.

--- src/test/java/com/oraculum/load/service/impl/SecHoldingFileLoadServiceImplTest.java
+++ src/test/java/com/oraculum/load/service/impl/SecHoldingFileLoadServiceImplTest.java
@@ -11,14 +11,14 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.time.LocalDate;
 import java.time.ZonedDateTime;
 
-import org.springframework.jdbc.core.JdbcTemplate;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
